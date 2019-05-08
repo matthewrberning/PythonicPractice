dcd29725218faf36a858cc4cebb51bb04b597b44
matthewrberning <40922361+matthewrberning@users.noreply.github.com>
2019-05-08 16:34:08 -0400

    list comprehension update

---
+++ list-comprehensions.py
@@ -0,0 +1,18 @@
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# we want n for each n in nums
+
+# my_list = []
+# for n in nums:
+#     my_list.append(n)
+# print(my_list)
+
+# my_list = [n for n in nums]
+# print(my_list)
+
+# want n*n for each n in nums
+
+my_list = []
+for n in nums:
+    my_list.append(n * n)
+print(my_list)
