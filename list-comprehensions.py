98d66868e3967806f1b1f8bc3ffadd8500d02c76
matthewrberning <40922361+matthewrberning@users.noreply.github.com>
2019-05-09 12:32:18 -0400

    neww list comprehensioons notebook update

--- list-comprehensions.py
+++ list-comprehensions.py
@@ -12,7 +12,21 @@ nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # want n*n for each n in nums
 
-my_list = []
-for n in nums:
-    my_list.append(n * n)
+# my_list = []
+# for n in nums:
+#     my_list.append(n * n)
+# print(my_list)
+
+# my_list = [n * n for n in nums]
+# print(my_list)
+
+# using a map + lambda
+my_list = map(lambda n: n * n, nums)
 print(my_list)
+
+# i want n for each n in nums if n is even
+# my_list = []
+# for n in nums:
+#     if n % 2 == 0:
+#         my_list.append(n)
+# print(my_list)
